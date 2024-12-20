f4d08f27c84063bfb60074691effd76890495b4e
gforcode <sm718036@gmail.com>
2024-12-20 12:46:27 +0500

    just removed main app db connection and set it to imported db connection

--- src/index.js
+++ src/index.js
@@ -6,20 +6,3 @@ dotenv.config({
 })
 
 connectDB()
-
-
-/* import express from 'express'
-const app = express()
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-    app.on('error', (error) => {
-      console.log('ERROR: ', error)
-    })
-    app.listen(process.env.PORT, () => {
-      console.log(`App is listening on port: ${process.env.PORT}`)
-    })
-  } catch (error) {
-    console.log("ERROR: ", error)
-  }
-})() */
