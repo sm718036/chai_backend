3b7bda4015e805a586164527c9843d784c7f9193
gforcode <sm718036@gmail.com>
2024-12-20 12:39:24 +0500

    connected database

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import connectDB from "./db/index.js";
+import dotenv from 'dotenv'
+
+dotenv.config({
+  path: './env'
+})
+
+connectDB()
+
+
+/* import express from 'express'
+const app = express()
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+    app.on('error', (error) => {
+      console.log('ERROR: ', error)
+    })
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port: ${process.env.PORT}`)
+    })
+  } catch (error) {
+    console.log("ERROR: ", error)
+  }
+})() */
